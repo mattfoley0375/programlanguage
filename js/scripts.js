6623ceb3128b18f87dea6858e511919f50672809
Matt Foley <mattfoley0375@gmail.com>
2021-01-15 13:36:54 -0800

    get rid of picture inserts for now and change user logic for if else as well as fix bug for id=language in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function results(input1, input2, input3, input4, input5) {
   const total = input1 + input2 + input3 + input4 + input5;
 
-  if(total <= 7){
+  if(total <= 7) {
     language = "Ruby";
-  } else if(total <= 11){
+  } else if(total <= 11) {
     language = "Python";
   } else if (total <= 15) {
     language = "React"
@@ -18,28 +18,30 @@ $(document).ready(function() {
   $("form#lang").submit(function() {
     event.preventDefault();
 
-    const work = parseInt($("input:radio(name=work)").val());
-    const style = parseInt($("input:radio(name=style)").val());
-    const movie = parseInt($("input:radio(name=movie)").val());
-    const music = parseInt($("input:radio(name=music)").val());
-    const season = parseInt($("input:radio(name=season)").val());
+    const work = parseInt($("input:radio[name=work]").val());
+    const style = parseInt($("input:radio[name=style]").val());
+    const movie = parseInt($("input:radio[name=movie]").val());
+    const music = parseInt($("input:radio[name=music]").val());
+    const season = parseInt($("input:radio[name=season]").val());
     let language = results(work, style, movie, music, season)
 
     if (language === "Ruby") {
-      $("#output").show();
-      $("#answer").text("Ruby");
-      $(".pic").hide();
-      $("#Ru").show(); 
+      $("#answer").hide();
+      $("#answer").show();
+      $("#language").text("Ruby");
     } else if (language === "Python") {
-      $("#output").show();
-      $("#answer").text("Python");
-      $(".pic").hide();
-      $("#Py").show();
+      $("#answer").hide();
+      $("#answer").show();
+      $("#language").text("Python");
+    } else if (language === "React") {
+      $("#answer").hide();
+      $("#answer").show();
+      $("#languageResults").text("React");
     } else {
-      $("#output").show();
-      $("#answer").text("React");
-      $(".pic").hide();
-      $("#Re").show();
+      $("#answer").hide();
+      $("#answer").show();
+      alert('FILL OUT THE SURVEY!');
     }
+    $("#answer").show();
   });
 });
