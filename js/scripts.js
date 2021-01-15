2c413034b663bb331fb718599fcffe2f6cdeeb51
Matt Foley <mattfoley0375@gmail.com>
2021-01-15 13:07:16 -0800

    fix number of inputs in function results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-function results(input1, input2, input3) {
-  const total = input1 + input2 + input3;
+function results(input1, input2, input3, input4, input5) {
+  const total = input1 + input2 + input3 + input4 + input5;
 
   if(total <= 5){
     language = "Ruby";
