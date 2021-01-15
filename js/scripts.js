43b58d1732a0df5867454e67306a20df25c05cd0
Matt Foley <mattfoley0375@gmail.com>
2021-01-15 10:06:48 -0800

    add fourth question about music

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,4 @@ $(document).ready(function() {
     const work = parseInt($("#work").val());
     const style = parseInt($("#style").val());
     const movie = parseInt($("#movie").val());
+    const music = parseInt($("#music").val());
